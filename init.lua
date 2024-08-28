17ac64e8d1ccfdb53915a93aa6e3ddf976a9f91b
VladislavMakhov <makhov1@list.ru>
2024-08-28 19:33:28 +0300

    vscode

--- init.lua
+++ init.lua
@@ -57,7 +57,7 @@ vim.o.smartcase = true
 vim.o.showmode = false
 
 -- Normal mode mapping for 'a'
-vim.api.nvim_set_keymap('n', 'a', 'i', { noremap = true })
+-- vim.api.nvim_set_keymap('n', 'a', 'i', { noremap = true })
 
 -- Plugin management with vim-plug
 
@@ -76,10 +76,10 @@ Plug 'daltonmenezes/aura-theme', { 'rtp': 'packages/neovim' }
 Plug 'gruvbox-community/gruvbox'
 Plug 'tpope/vim-fugitive'
 Plug 'windwp/nvim-autopairs'
+Plug 'Mofiqul/vscode.nvim'
 
 call plug#end()
 
-colorscheme gruvbox
 set background=dark
 ]])
 
@@ -150,3 +150,41 @@ end
 
 remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
 
+
+-- Lua:
+-- For dark theme (neovim's default)
+vim.o.background = 'dark'
+
+local c = require('vscode.colors').get_colors()
+require('vscode').setup({
+    -- Alternatively set style in setup
+    -- style = 'light'
+
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Underline `@markup.link.*` variants
+    underline_links = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        vscLineNumber = '#FFFFFF',
+    },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+    }
+})
+-- require('vscode').load()
+
+-- load the theme without affecting devicon colors.
+vim.cmd.colorscheme "vscode"
