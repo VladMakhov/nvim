5907c90f73aead4cfc50d04445bafe440dc7792a
VladislavMakhov <makhov1@list.ru>
2024-09-02 17:47:55 +0300

    0209

--- init.lua
+++ init.lua
@@ -1,26 +1,16 @@
-vim.api.nvim_set_keymap('n', 'tt', ':lua tsp()<CR>', { noremap = true })
-
+-- KEYMAPS
+vim.api.nvim_set_keymap('n', 'tt', ':TransparentToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', 'qq', '<Esc>l', { noremap = true })
 vim.api.nvim_set_keymap('i', 'QQ', '<Esc>l', { noremap = true })
 vim.api.nvim_set_keymap('i', '<S-Del>', '<Esc>ldwi', { noremap = true })
-
--- Highlight search and jump to next occurrence
 vim.api.nvim_set_keymap('n', '@', ":let @/='\\<'.expand('<cword>').'\\>'<CR>:set hlsearch<CR>*", { noremap = true })
 vim.api.nvim_set_keymap('n', '#', ":let @/='\\<'.expand('<cword>').'\\>'<CR>:set hlsearch<CR>#", { noremap = true })
-
--- Disable highlight search with 'ff'
 vim.api.nvim_set_keymap('n', 'ff', ':nohlsearch<CR>', { noremap = true })
-
--- Undo with 'q'
 vim.api.nvim_set_keymap('n', 'q', 'u', { noremap = true })
-
--- Telescope mappings
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true })
-
--- Insert mode mappings for completion
 vim.api.nvim_set_keymap('i', '<TAB>', 'pumvisible() ? "\\<C-n>" : "\\<TAB>"', { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('i', '<S-TAB>', 'pumvisible() ? "\\<C-p>" : "\\<S-TAB>"', { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('i', '<CR>', 'pumvisible() ? coc#_select_confirm() : "\\<CR>"', { noremap = true, expr = true, silent = true })
@@ -31,10 +21,7 @@ vim.api.nvim_set_keymap('n', 'L', 'w', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'L', 'w', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'J', 'j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'K', 'k', { noremap = true, silent = true })
-
 vim.keymap.set('n', 'mm', vim.cmd.UndotreeToggle)
-
--- Settings
 vim.cmd('syntax on')
 vim.o.swapfile = false
 vim.o.encoding = 'utf-8'
@@ -58,13 +45,14 @@ vim.o.mouse = 'a'
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.showmode = false
-vim.g.undotree_SplitWidth = 30
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.o.background = 'dark'
+-- KEYMAPS
 
--- Normal mode mapping for 'a'
--- vim.api.nvim_set_keymap('n', 'a', 'i', { noremap = true })
 
--- Plugin management with vim-plug
-
+-- PLUGINS
 vim.cmd([[
 call plug#begin('~/.config/nvim/plugged')
 
@@ -83,34 +71,41 @@ Plug 'windwp/nvim-autopairs'
 Plug 'Mofiqul/vscode.nvim'
 Plug 'https://github.com/folke/tokyonight.nvim'
 Plug 'numToStr/Comment.nvim'
-
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-
 Plug 'https://github.com/mbbill/undotree'
 Plug 'rose-pine/neovim'
+Plug 'tribela/transparent.nvim'
 
 call plug#end()
-
 colorscheme rose-pine-main
 set background=dark
 ]])
+-- PLUGINS
 
--- Plug 'tribela/transparent.nvim'
--- vim.g.transparent_enabled = 0
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- SETUP
+local npairs = require('nvim-autopairs')
+local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
+local is_transparent = false
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+require("transparent").setup({
+  groups = {
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+    'EndOfBuffer',
+  },
+  extra_groups = {},
+  exclude_groups = {},
+  on_clear = function() end,
+})
 
 require('Comment').setup()
 
--- empty setup using defaults
 require("nvim-tree").setup()
 
--- OR setup with some options
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
@@ -126,10 +121,6 @@ require("nvim-tree").setup({
   },
 })
 
-local npairs = require('nvim-autopairs')
-local Rule = require('nvim-autopairs.rule')
-local cond = require('nvim-autopairs.conds')
-
 npairs.setup({
     check_ts = true,
     ts_config = {
@@ -139,7 +130,6 @@ npairs.setup({
     }
 })
 
--- Integrate with coc.nvim
 npairs.add_rules({
     Rule("(", ")", "lua")
         :with_pair(cond.not_after_regex_check("%%"))
@@ -149,7 +139,6 @@ npairs.add_rules({
         :with_cr(cond.none())
 })
 
--- If you want insert `(` after select function or method item
 local remap = vim.api.nvim_set_keymap
 _G.MUtils= {}
 
@@ -163,43 +152,25 @@ end
 
 remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
 
-
--- Lua:
--- For dark theme (neovim's default)
-vim.o.background = 'dark'
-
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
-    -- Alternatively set style in setup
-    -- style = 'light'
-
-    -- Enable transparent background
     transparent = true,
 
-    -- Enable italic comment
     italic_comments = true,
 
-    -- Underline `@markup.link.*` variants
     underline_links = true,
 
-    -- Disable nvim-tree background color
     disable_nvimtree_bg = true,
 
-    -- Override colors (see ./lua/vscode/colors.lua)
     color_overrides = {
         vscLineNumber = '#FFFFFF',
     },
 
-    -- Override highlight groups (see ./lua/vscode/theme.lua)
     group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
         Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
     }
 })
 -- require('vscode').load()
-
--- load the theme without affecting devicon colors.
 -- vim.cmd.colorscheme "vscode"
 
 
@@ -211,9 +182,6 @@ require'nvim-treesitter.configs'.setup {
 
   auto_install = true,
 
-  -- ignore_install = { "javascript" },
-
-
   highlight = {
     enable = true,
 
@@ -221,23 +189,19 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
--- Функция для установки фиксированной ширины undotree
 local function set_undotree_width()
 vim.cmd('vertical resize 50')
 end
 
--- Функция для установки фиксированной высоты diffpanel_3
 local function set_diffpanel_height()
 vim.cmd('resize 20')
 end
 
--- Автокоманда для установки ширины при открытии undotree
 vim.api.nvim_create_autocmd('BufWinEnter', {
 pattern = 'undotree_*',
 callback = set_undotree_width,
 })
 
--- Автокоманда для установки ширины при изменении размера окна
 vim.api.nvim_create_autocmd('VimResized', {
 pattern = '*',
 callback = function()
@@ -247,13 +211,11 @@ callback = function()
 end,
 })
 
--- Автокоманда для установки высоты при открытии diffpanel_3
 vim.api.nvim_create_autocmd('BufWinEnter', {
 pattern = 'diffpanel_3',
 callback = set_diffpanel_height,
 })
 
--- Автокоманда для установки высоты при изменении размера окна
 vim.api.nvim_create_autocmd('VimResized', {
 pattern = '*',
 callback = function()
@@ -263,7 +225,6 @@ callback = function()
 end,
 })
 
-local is_transparent = false
 
 function tsp(color)
     color = color or "rose-pine-main"
@@ -279,4 +240,4 @@ function tsp(color)
 
     is_transparent = not is_transparent
 end
-
+-- SETUP
