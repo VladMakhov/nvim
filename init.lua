efe4f0b734951a124055a8df5cc2a90ea54a890c
VladislavMakhov <makhov1@list.ru>
2024-09-09 18:51:45 +0300

    0909

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 require("core.keymaps")
 require("core.plugins")
-require("core.config.rosepine")
+require("core.config.colors")
 require("core.config.autopairs")
 require("core.config.telescope")
 require("core.config.treesitter")
 require("core.config.fileexplorer")
 require("core.config.transparent")
 require("core.config.lsp")
+
--- lua/core/config/rosepine.lua
+++
@@ -1,6 +0,0 @@
-vim.o.termguicolors = true
-require('rose-pine').setup({
-     disable_italics = true
-})
--- vim.cmd [[ colorscheme rose-pine-main ]]
-vim.cmd [[ colorscheme no-clown-fiesta]]
